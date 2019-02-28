b78138c6dfde07e60402aa8c37e19147cf0e62d6
taisaoko <ttd3calvin@gmail.com>
2019-02-28 22:53:52 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,6 @@
 class Show < ActiveRecord::Base
   def Show.highest_rating
-    Show.maximum("rating")
+    Show.maximum(:rating)
   end
 
   def Show.most_popular_show
@@ -8,7 +8,7 @@ class Show < ActiveRecord::Base
   end
 
   def Show.lowest_rating
-    Show.minimum("rating")
+    Show.minimum(:rating)
   end
 
   def Show.least_popular_show
